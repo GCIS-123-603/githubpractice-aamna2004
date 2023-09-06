75fa0897150658196bbf375a0dde4ddcc8eacaa8
aamna2004 <143425740+aamna2004@users.noreply.github.com>
2023-09-06 15:51:39 +0400

    Add files via upload
    import turtle thingy

---
+++ turtlestuff.py
@@ -0,0 +1,15 @@
+import turtle 
+
+def testdrive():
+    turtle.forward(100)
+    turtle.left(87)
+    turtle.setheading(127)
+    turtle.down()
+    turtle.goto(50, 50)
+    turtle.home()
+    turtle.circle(25)
+
+def main():
+    testdrive()
+
+main()
