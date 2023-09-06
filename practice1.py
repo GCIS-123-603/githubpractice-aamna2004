5a819befb6769e5adb22fbb958d7c9ab575b8074
aamna2004 <143425740+aamna2004@users.noreply.github.com>
2023-09-06 14:32:53 +0400

    Add files via upload
    "version 2 "

--- practice1.py
+++ practice1.py
@@ -1,5 +1,16 @@
-name=input("enter name : ")
-birthmonth=input("enter birth month : ")
-day=int(input("enter day of your birthday :"))
-year=int(input("enter year of your birthday :"))
-print(name , "your birthday is on " , birthmonth , day , " , " , year ,"!")
+#2.2.2 Activity
+
+def bday_message():
+    name=input("enter name : ")
+    birthmonth=input("enter birth month : ")
+    day=int(input("enter day of your birthday :"))
+    year=int(input("enter year of your birthday :"))
+    print(name , "your birthday is on " , birthmonth, day , " , " , year ,"!")
+
+
+bday_message()
+
+
+#end
+
+
