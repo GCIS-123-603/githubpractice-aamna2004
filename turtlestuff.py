78b98f74367635755bf7bf4ec46e0bdf6dbf405b
aamna2004 <143425740+aamna2004@users.noreply.github.com>
2023-09-11 13:34:30 +0400

    turtlestuff.py

--- turtlestuff.py
+++ turtlestuff.py
@@ -9,7 +9,19 @@ def testdrive():
     turtle.home()
     turtle.circle(25)
 
+def turtle_state():
+    print("Is Turtle down ?" ,turtle.isdown()) #checking whether the turtle pen is down or up
+    print()
+    print("Where is turtle heading ?" ,turtle.heading())
+    print()
+    print("The X coordinate of turtle is " ,turtle.xcor()) # gives x coordinate
+    print()
+    print("The y coordinate of turtle is ",turtle.ycor()) # gives y coordinate
+
 def main():
-    testdrive()
+    testdrive() 
+    turtle_state()
 
 main()
+turtle.done() # to keep turtle window open after execution 
+
