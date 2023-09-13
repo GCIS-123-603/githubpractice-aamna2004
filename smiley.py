bf8c728f1a6d74de989bcd9ace3e1919c5a0b21b
aamna2004 <143425740+aamna2004@users.noreply.github.com>
2023-09-13 15:28:14 +0400

    smiley.py

--- smiley.py
+++ smiley.py
@@ -1,23 +1,78 @@
-import turtle
+import turtle as t
 
-turtle=turtle.Turtle()
+screen = t.Screen()
+screen.setup(width=1000, height=1000)     
+screen.bgcolor("white")
 
-def draw_circle():
-    turtle.color('red','blue')
-    turtle.pensize(1)
-    turtle.begin_fill()
-    turtle.circle(50)
-    turtle.end_fill()
+# создаем черепашку
+pen = t.Turtle()
+pen.speed(20)
 
-def draw_centered_circle():
-    #step1 get user input on radius
-    #step2 start fill
-    #step3 use turtle.circle(radius)
-    #step4 end fill
-    
+#face
+pen.penup()
+pen.goto(0, -200)                       
+pen.pendown()
+pen.begin_fill()                        
+pen.color("yellow")                     
+pen.circle(200)                         
+pen.end_fill()                          
 
-def main():
-    draw_circle()
-    input("enter to continue..")
+#eye
+pen.penup()
+pen.goto(-80, 50)                       
+pen.pendown()
+pen.begin_fill()                       
+pen.color("white")                      
+pen.circle(40)                           
+pen.end_fill()                         
 
-main()
+pen.penup()
+pen.goto(80, 50)
+pen.pendown()
+pen.begin_fill()
+pen.color("white")
+pen.circle(40)
+pen.end_fill()
+
+#pupils
+pen.penup()
+pen.goto(-80, 80)
+pen.pendown()
+pen.begin_fill()
+pen.color("black")
+pen.circle(20)
+pen.end_fill()
+
+pen.penup()
+pen.goto(80, 80)
+pen.pendown()
+pen.begin_fill()
+pen.color("black")
+pen.circle(20)
+pen.end_fill()
+
+#smile
+pen.setheading(270)
+
+pen.penup()
+pen.goto(-100, -30)
+pen.pendown()
+pen.begin_fill()
+pen.color("black")
+pen.circle(100, 180)
+pen.end_fill()
+
+pen.penup()
+pen.goto(10, 0)
+pen.pendown()
+pen.begin_fill()
+pen.color("orange")
+pen.circle(10)
+pen.end_fill()
+
+
+
+pen.hideturtle()                        
+
+t.done()
+
